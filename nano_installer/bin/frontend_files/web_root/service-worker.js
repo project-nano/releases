d6c2bb39a0392701586b0da461f201bf91a4cc80
bokuore <6408741@qq.com>
2023-11-19 12:21:07 +0800

    update installer&web_root

--- nano_installer/bin/frontend_files/web_root/service-worker.js
+++ nano_installer/bin/frontend_files/web_root/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1aff4a0c2475d969f0bdc6822387a682.js"
+  "/precache-manifest.894bb590fa8ff419fafc579ff8334eb9.js"
 );
 
 self.addEventListener('message', (event) => {
